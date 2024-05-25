495840c4582474e69961cfb15bb1bd15cd5f412c
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2024-05-25 10:52:30 +0530

    Create OptionalEmptyDemo.java

---
+++ java8/code/optionals/OptionalEmptyDemo.java
@@ -0,0 +1,10 @@
+package practice.java8Demo.Optionals;
+
+import java.util.Optional;
+
+public class OptionalEmptyDemo {
+	public static void main(String[] args) {
+	Optional<Object> output = Optional.empty();
+	System.out.println(output); //Optional.empty
+	}
+}
