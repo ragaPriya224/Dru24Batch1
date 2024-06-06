a4084eefd0b3782f270ccdf0e9764cf9fe5d7361
priya <priya@zawqwe.com>
2024-06-06 15:52:28 +0530

    get particular topic

--- spring/ApiDemo/src/main/java/com/druCare/apiDemo/controller/TopicController.java
+++ spring/ApiDemo/src/main/java/com/druCare/apiDemo/controller/TopicController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.druCare.apiDemo.entity.Topic;
@@ -21,4 +22,9 @@ public class TopicController {
 		return service.getAllTopic();
 	}
 	
+	
+	@GetMapping("/topic/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return service.getTopic(id);
+	}
 }
--- spring/ApiDemo/src/main/java/com/druCare/apiDemo/service/TopicService.java
+++ spring/ApiDemo/src/main/java/com/druCare/apiDemo/service/TopicService.java
@@ -10,14 +10,30 @@ import com.druCare.apiDemo.entity.Topic;
 
 @Service
 public class TopicService {
-
+	List<Topic> topicList = new ArrayList(Arrays.asList(
+			new Topic("11","fantasy movie","harrypotter"),
+			new Topic("33","basic knowledge of java","java dummies"),
+			new Topic("22","adv java","sb")
+			));
+	
 	public List<Topic> getAllTopic() {
-		List<Topic> topicList = new ArrayList(Arrays.asList(
-				new Topic("11","fantasy movie","harrypotter"),
-				new Topic("33","basic knowledge of java","java dummies"),
-				new Topic("22","adv java","sb")
-				));
 		return topicList;
 	}
 
+	public Topic getTopic(String id) {
+	Topic t = new Topic();
+		for(int i = 0; i<topicList.size();i++) {
+			if(topicList.get(i).getId().equals(id)) {
+				return topicList.get(i);
+			}
+		}
+		return t;
+	}
+
+	
+	public void m1() {
+		String[] name = new String[] {"anusha","anuhya","uma","dhruv","meg","venkt"};
+		
+		
+	}
 }
