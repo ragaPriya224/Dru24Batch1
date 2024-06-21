05909c0026d4ec13dc7f7e16325f7f25d17cb232
priya <priya@zawqwe.com>
2024-06-21 16:37:38 +0530

    ms:got hardcoded data for catalogue

--- movieCatalogService/movieCatalogService/src/main/java/com/dru24batch/movieCatalogService/MovieCatalogController.java
+++ movieCatalogService/movieCatalogService/src/main/java/com/dru24batch/movieCatalogService/MovieCatalogController.java
@@ -1,7 +1,8 @@
 package com.dru24batch.movieCatalogService;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,8 +14,16 @@ public class MovieCatalogController {
 	//hardcoded list of movie +ratings info
 	@GetMapping("/catalog/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable String userId){
-		return Collections.singletonList(
-				new CatalogItem("jamesbond","investigation" ,4)
-				);
+//		return Collections.singletonList(
+//				new CatalogItem("jamesbond","investigation" ,4)
+//				);
+		
+		List<Rating> ratingsList = Arrays.asList(
+				new Rating("123",4),
+				new Rating("456",3));  // assume this the response we got from ratingsdata api 
+		
+		return 	ratingsList.stream().map(ratings ->{
+				return new CatalogItem("transformer","automobiles" ,4);// must come from api 
+			}).collect(Collectors.toList());
 	}
 }
