36bbbe9b9d733ce802b02da571a4a98af8e0ae80
priya <priya@zawqwe.com>
2024-06-24 16:50:47 +0530

    ms: non recommended listrating return

--- ratingsDataService/ratingsDataService/src/main/java/com/dru24batch/ratingsDataService/RatingsController.java
+++ ratingsDataService/ratingsDataService/src/main/java/com/dru24batch/ratingsDataService/RatingsController.java
@@ -1,15 +1,29 @@
 package com.dru24batch.ratingsDataService;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class RatingsController {
 
-	
+
 	@GetMapping("/ratingsdata/{id}")
 	public Rating getRating(@PathVariable String id) {
 		return new Rating(id, 4);
 	}
+	//userid -? movieId's
+
+	@GetMapping("/users/{userId}")
+	public List<Rating>   getUserRating(@PathVariable String userId) {
+		List<Rating> ratingsList = Arrays.asList(
+				new Rating("123",4), //movie id, rating
+				new Rating("456",3));  // assume this the response we got from ratingsdata api 
+		return ratingsList;
+	}
+
 }
