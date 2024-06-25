bae8758d366f1968f216dfa63c21322bd0313577
priya <priya@zawqwe.com>
2024-06-25 16:03:37 +0530

    ms:eureka server creation

---
+++ microservices/eurekaServer/eurekaServer/src/test/java/com/dru24batch1/eurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dru24batch1.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
