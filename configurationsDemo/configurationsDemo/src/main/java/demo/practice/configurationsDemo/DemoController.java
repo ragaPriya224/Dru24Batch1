c2f949575d4fe7610ac117b301bbe1e6cfd901a0
priya <priya@zawqwe.com>
2024-06-28 14:32:52 +0530

    app:values

--- configurationsDemo/configurationsDemo/src/main/java/demo/practice/configurationsDemo/DemoController.java
+++ configurationsDemo/configurationsDemo/src/main/java/demo/practice/configurationsDemo/DemoController.java
@@ -1,13 +1,18 @@
 package demo.practice.configurationsDemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+	
+	@Value("${my.greeting : novaluepresent}")
+	private String greetingMessage;
+	
 	@GetMapping("/team")
 	public String m1() {
-		return "works";
+		return greetingMessage;
 	}
 }
