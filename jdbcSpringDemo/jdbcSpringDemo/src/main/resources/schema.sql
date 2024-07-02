3cf4701dbb3110a0fdf64ec3496e31b2cc81395c
priya <priya@zawqwe.com>
2024-07-02 16:40:08 +0530

    insert sql

--- jdbcSpringDemo/jdbcSpringDemo/src/main/resources/schema.sql
+++ jdbcSpringDemo/jdbcSpringDemo/src/main/resources/schema.sql
@@ -5,3 +5,14 @@ description varchar(100) NOT NULL,
 link varchar(255) NOT NULL,
 CONSTRAINT pk_course_course_id  primary key (course_id)
 );
+
+
+INSERT INTO course
+VALUES(11,"Java","Learning java","java.com");
+INSERT INTO COURSE(course_id,title,description,link)
+VALUES(1,"JAVA","I am learning java","https://meet.google.com/vra-kwwf-gjj");
+
+INSERT INTO drubatch1.course (course_id, title, description, link)
+VALUES (4, 'Introduction to Programming', 'basics of programming', 'http://example.com/intro-to-programming'),
+(2, 'Database', 'database architectures and designs', 'http://example.com/advanced-db-systems'),
+(3, 'Python', 'Build web applications using Python', 'http://example.com/web-dev-python');
