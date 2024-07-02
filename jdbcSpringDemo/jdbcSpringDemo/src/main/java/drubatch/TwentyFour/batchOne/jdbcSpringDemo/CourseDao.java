a96384a35d3b86b5dd943079a24e7d03c080f1b0
priya <priya@zawqwe.com>
2024-07-02 16:32:52 +0530

    crud op with jdbctemplate

--- jdbcSpringDemo/jdbcSpringDemo/src/main/java/drubatch/TwentyFour/batchOne/jdbcSpringDemo/CourseDao.java
+++ jdbcSpringDemo/jdbcSpringDemo/src/main/java/drubatch/TwentyFour/batchOne/jdbcSpringDemo/CourseDao.java
@@ -2,15 +2,18 @@ package drubatch.TwentyFour.batchOne.jdbcSpringDemo;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class CourseDao {
 
 	JdbcTemplate jdbcTemplate;
-
+	private static final Logger log = LoggerFactory.getLogger(CourseDao.class);
 	public CourseDao(JdbcTemplate jdbcTemplate) {
 		super();
 		this.jdbcTemplate = jdbcTemplate;
@@ -25,12 +28,38 @@ public class CourseDao {
 		return course;
 	};
 	
-	public List<Course> getList(){
+	public List<Course> getCourseList(){
 		String sql = "SELECT course_id, title, description,link  FROM course";
 		return jdbcTemplate.query(sql, rowMapper);
 	}
+	public void create(Course course) {
+		String sql = "INSERT INTO course values(?,?,?,?)";
+		int output = jdbcTemplate.update(sql,course.getCourseId(),course.getTitle(),
+				course.getDescription(),course.getLink());
+		if(output == 1) {
+			log.info("new course is created");
+		}
+	}
 	
+	public void update(Course course,int id) {
+		String sql = "UPDATE  course SET title = ?,description=? where course_id = ?";
+		int output = jdbcTemplate.update(sql,course.getTitle(),
+				course.getDescription(),course.getCourseId());
+		if(output == 1) {
+			log.info("updated  course ");
+		}
+	}
+	public Course getCourse(int id){
+		String sql = "SELECT course_id, title, description,link  FROM course where course_id = ?";
+		return jdbcTemplate.queryForObject(sql, rowMapper,new Object[] {id});
+	}
 	
-	
+	public void delCourse(int id){
+		String sql = "DELETE FROM course where course_id = ?";
+		int output =  jdbcTemplate.update(sql,id);
+		if(output == 1) {
+			log.info("deleted  course ");
+		}
+	}
 	
 }
--- jdbcSpringDemo/jdbcSpringDemo/src/main/java/drubatch/TwentyFour/batchOne/jdbcSpringDemo/JdbcSpringDemoApplication.java
+++ jdbcSpringDemo/jdbcSpringDemo/src/main/java/drubatch/TwentyFour/batchOne/jdbcSpringDemo/JdbcSpringDemoApplication.java
@@ -15,7 +15,17 @@ public class JdbcSpringDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JdbcSpringDemoApplication.class, args);
 		System.out.println("*************");
-		System.out.println(dao.getList());
+		System.out.println(dao.getCourseList());
+		System.out.println("******get particular data*******");
+		System.out.println(dao.getCourse(1));
+		System.out.println("******create  data*******");
+		Course c = new Course(22,"english","spoken class for formal","www.dummy");
+		dao.create(c);
+		System.out.println("******update  data*******");
+		Course c1 = new Course(4,"updated","upd desfcription","www.upddummy");
+		dao.update(c1, 4);
+		System.out.println("********************delete");
+		dao.delCourse(3);
 	}
 	
 
