97031942ca9e189facf9b87745a4b3c3bfafa666
priya <priya@zawqwe.com>
2024-07-02 15:21:37 +0530

    jdbctemplate

---
+++ jdbcSpringDemo/jdbcSpringDemo/src/main/java/drubatch/TwentyFour/batchOne/jdbcSpringDemo/JdbcSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package drubatch.TwentyFour.batchOne.jdbcSpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcSpringDemoApplication.class, args);
+	}
+
+}
---
+++ jdbcSpringDemo/jdbcSpringDemo/src/test/java/drubatch/TwentyFour/batchOne/jdbcSpringDemo/JdbcSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package drubatch.TwentyFour.batchOne.jdbcSpringDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
