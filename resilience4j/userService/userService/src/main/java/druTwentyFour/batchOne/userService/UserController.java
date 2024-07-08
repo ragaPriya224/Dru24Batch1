956f0b9ccc2618550dd43ea47b5f84120fff07dc
priya <priya@zawqwe.com>
2024-07-08 15:36:13 +0530

    res:pom,app.props,falback

--- resilience4j/userService/userService/src/main/java/druTwentyFour/batchOne/userService/UserController.java
+++ resilience4j/userService/userService/src/main/java/druTwentyFour/batchOne/userService/UserController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 
 @RestController
 @RequestMapping("/user-service")
@@ -25,23 +27,20 @@ public class UserController {
 	private static final String BASEURL = "http://localhost:9191/orders";
 
 	@GetMapping("/displayOrders")
+	@CircuitBreaker(name =USER_SERVICE,fallbackMethod = "getAllAvailableProducts" )
 	public List<OrderDTO> displayOrders() {
 		return restTemplate.getForObject(BASEURL, ArrayList.class);
 	}
 	
-	
-	
-//	@CircuitBreaker(name =USER_SERVICE,fallbackMethod = "getAllAvailableProducts")
-
-//	//note return type should be same as the original method
-//	public List<OrderDTO> getAllAvailableProducts(Exception e){
-//		return Stream.of(
-//				new OrderDTO(111,"pen","stationary","green",9999),
-//				new OrderDTO(345, "Headset", "electronics", "black", 7000),
-//				new OrderDTO(475, "Sound bar", "electronics", "black", 13000),
-//				new OrderDTO(574, "Puma Shoes", "foot wear", "black & white", 4600),
-//				new OrderDTO(678, "Vegetable chopper", "kitchen", "blue", 999),
-//				new OrderDTO(532, "Oven Gloves", "kitchen", "gray", 745)
-//				).collect(Collectors.toList());
-//	}
+	//note return type should be same as the original method
+	public List<OrderDTO> getAllAvailableProducts(Exception e){
+		return Stream.of(
+				new OrderDTO(111,"pen","stationary","green",9999),
+				new OrderDTO(345, "Headset", "electronics", "black", 7000),
+				new OrderDTO(475, "Sound bar", "electronics", "black", 13000),
+				new OrderDTO(574, "Puma Shoes", "foot wear", "black & white", 4600),
+				new OrderDTO(678, "Vegetable chopper", "kitchen", "blue", 999),
+				new OrderDTO(532, "Oven Gloves", "kitchen", "gray", 745)
+				).collect(Collectors.toList());
+	}
 }
