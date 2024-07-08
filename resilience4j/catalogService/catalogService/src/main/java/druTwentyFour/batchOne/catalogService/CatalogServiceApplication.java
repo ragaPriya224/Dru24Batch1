3027efce356c71fdcf75431602546254b89f6310
priya <priya@zawqwe.com>
2024-07-08 14:28:38 +0530

    res: catalog,userservice project creation

---
+++ resilience4j/catalogService/catalogService/src/main/java/druTwentyFour/batchOne/catalogService/CatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package druTwentyFour.batchOne.catalogService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceApplication.class, args);
+	}
+
+}
---
+++ resilience4j/catalogService/catalogService/src/test/java/druTwentyFour/batchOne/catalogService/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package druTwentyFour.batchOne.catalogService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
